40e90f5a85ff7f94411fa56c0f8e3f826cce8f01
AkshatM17 <a.maheshwari@thesynapses.com>
2023-06-19 19:03:51 +0530

    added a basic strucure of the cards and data for the products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/layouts/navbar";
+import { Products } from "./pages/products";
+import { Cart } from "./pages/cart";
+
 
 function App() {
   return (
     <div className="App">
-      
+       <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/cart" element={<Cart/>} />
+        </Routes>
+       </Router>
     </div>
   );
 }
---
+++ src/pages/productDetails.js
@@ -0,0 +1,50 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 2,
+        productName: "Macbook",
+        price: 1999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 3,
+        productName: "Ipad",
+        price: 9990.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 4,
+        productName: "Ipad Mini",
+        price: 11999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 5,
+        productName: "Apple Watch",
+        price: 999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 6,
+        productName: "Iphone 12 pro",
+        price: 999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 7,
+        productName: "Iphone 13 pro ",
+        price: 999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+    {
+        id: 8,
+        productName: "Iphone 14 pro",
+        price: 999.0,
+        productImage: "https://via.placeholder.com/600/92c952",
+    },
+];
