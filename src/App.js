4985409514314d179eaac49f4ebdd981c3da7c32
AkshatM17 <a.maheshwari@thesynapses.com>
2023-06-23 16:12:28 +0530

    Minor Modificatons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Router, Route, Link, BrowserRouter as Routers } from "react-router-dom";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 import AddProduct from './Components/AddProduct';
 import Cart from './Components/Cart';
@@ -14,90 +16,189 @@ export default class App extends Component {
     this.state = {
       user: null,
       cart: {},
-      products: []
+      products: [],
+      showMenu: false
     };
     this.routerRef = React.createRef();
   }
 
+  async componentDidMount() {
+    let user = localStorage.getItem("user");
+    let cart = localStorage.getItem("cart");
+
+    const products = await axios.get('https://fakestoreapi.com/products');
+    user = user ? JSON.parse(user) : null;
+    cart = cart? JSON.parse(cart) : {};
+
+    this.setState({ user, products: products.data, cart });
+  }
+
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      'https://fakestoreapi.com/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  removeFromCart = cartItemId => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  clearCart = () => {
+    let cart = {};
+    localStorage.removeItem("cart");
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    if (!this.state.user) {
+      this.routerRef.current.history.push("/login");
+      return;
+    }
+
+    const cart = this.state.cart;
+
+    const products = this.state.products.map(p => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+
+        axios.put(
+          `https://fakestoreapi.com/products/`,
+          { ...p },
+        )
+      }
+      return p;
+    });
+
+    this.setState({ products });
+    this.clearCart();
+  };
+
+  
   render() {
-    return(
+    return (
       <Context.Provider
-      value={{
-        ...this.state,
-        removeFromCart: this.removeFromCart,
-        addToCart: this.addToCart,
-        login: this.login,
-        addProduct: this.addProduct,
-        clearCart: this.clearCart,
-        checkout: this.checkout
-      }}
-    >
-      <Router ref={this.routerRef}>
-      <div className="App">
-        <nav
-          className="navbar container"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div className="navbar-brand">
-            <b className="navbar-item is-size-4 ">ecommerce</b>
-            <label
-              role="button"
-              class="navbar-burger burger"
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarBasicExample"
-              onClick={e => {
-                e.preventDefault();
-                this.setState({ showMenu: !this.state.showMenu });
-              }}
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <BrowserRouter>
+          <div className="App">
+            <nav
+              className="navbar container"
+              role="navigation"
+              aria-label="main navigation"
             >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </label>
-          </div>
-            <div className={`navbar-menu ${
-                this.state.showMenu ? "is-active" : ""
-              }`}>
-              <Link to="/products" className="navbar-item">
-                Products
-              </Link>
-              {this.state.user && this.state.user.accessLevel < 1 && (
-                <Link to="/add-product" className="navbar-item">
-                  Add Product
-                </Link>
-              )}
-              <Link to="/cart" className="navbar-item">
-                Cart
-                <span
-                  className="tag is-primary"
-                  style={{ marginLeft: "5px" }}
+              <div className="navbar-brand">
+                <b className="navbar-item is-size-4 ">ecommerce</b>
+                <label
+                  role="button"
+                  className="navbar-burger burger"
+                  aria-label="menu"
+                  aria-expanded="false"
+                  data-target="navbarBasicExample"
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
                 >
-                  { Object.keys(this.state.cart).length }
-                </span>
-              </Link>
-              {!this.state.user ? (
-                <Link to="/login" className="navbar-item">
-                  Login
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </label>
+              </div>
+              <div className={`navbar-menu ${this.state.showMenu ? "is-active" : ""}`}>
+                <Link to="/products" className="navbar-item">
+                  Products
                 </Link>
-              ) : (
-                <Link to="/" onClick={this.logout} className="navbar-item">
-                  Logout
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    {Object.keys(this.state.cart).length}
+                  </span>
                 </Link>
-              )}
-            </div>
-          </nav>
-          <Router>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <Link to="/" onClick={this.logout} className="navbar-item">
+                    Logout
+                  </Link>
+                )}
+              </div>
+              </nav>
+              <Routes>
             <Route exact path="/" component={ProductList} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/cart" component={Cart} />
             <Route exact path="/add-product" component={AddProduct} />
             <Route exact path="/products" component={ProductList} />
-          </Router>
-        </div>
-      </Router>
-    </Context.Provider>
-  );
-}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </Context.Provider>
+    );
+  }
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import withContext from "../withContext";
 
-export default function Login() {
-  return <>Login</>
+const Login = (props) => {
+  const navigate = useNavigate();
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+    error: ""
+  });
+
+  const handleChange = e => setState({ ...state, [e.target.name]: e.target.value, error: "" });
+
+  const login = (e) => {
+    e.preventDefault();
+
+    const { username, password } = state;
+    if (!username || !password) {
+      return setState({ ...state, error: "Fill all fields!" });
+    }
+    props.context.login(username, password)
+      .then((loggedIn) => {
+        if (!loggedIn) {
+          setState({ ...state, error: "Invalid Credentials" });
+        }
+      });
+  };
+
+  if (props.context.user) {
+    navigate("/products");
+    return null;
+  }
+
+  return (
+    <>
+      <div className="hero is-primary ">
+        <div className="hero-body container">
+          <h4 className="title">Login</h4>
+        </div>
+      </div>
+      <br />
+      <br />
+      <form onSubmit={login}>
+        <div className="columns is-mobile is-centered">
+          <div className="column is-one-third">
+            <div className="field">
+              <label className="label">Email: </label>
+              <input
+                className="input"
+                type="email"
+                name="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label className="label">Password: </label>
+              <input
+                className="input"
+                type="password"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            {state.error && (
+              <div className="has-text-danger">{state.error}</div>
+            )}
+            <div className="field is-clearfix">
+              <button
+                className="button is-primary is-outlined is-pulled-right"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
+
+export default withContext(Login);
