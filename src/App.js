e674ad56373cae7e692e23f2fd00435bcab61daa
AkshatM17 <a.maheshwari@thesynapses.com>
2023-06-26 12:11:55 +0530

    created the products section, also fetched the products from the api and the description page for the products

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './modules/Home';
+import Footer from './Components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Product from './Components/Product';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/products/:id' element={<Product/>} />
+      </Routes>
+      <Footer />
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Hero  from '../../Components/Hero';
 import Products from '../../Components/Products';
+import FeatureCard from '../../Components/FeatureCard';
+import StatCard from '../../Components/StatCard';
+
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products?limit=12')
+      const data = await response.json()
+      console.log(data)
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
     <Hero />
-    <h2 className='text-6xl font-bold text-center mt-20'>Products </h2>
+    <div className='flex flex-col text-center w-full mt-20'>
+      <h2 className='text-xs text-indigo-500 tracking-widest font-medium title-font mb-1'>Products</h2>
+      <h1 className='sm:text-3xl text-2xl font-medium title-font text-gray-900'>Most Popular Products</h1>
+    </div>
+    {
+      products.length > 0 ?
+      <Products products={products} />
+      :
+      <div>Loading....</div>
+    }
     <Products />
+    <FeatureCard />
+    <StatCard />
     </>
   )
 }
