0ae0452358d68f6aac9f8ca6076f6a1793537772
AkshatM17 <a.maheshwari@thesynapses.com>
2023-06-26 13:47:58 +0530

    added cart functionality and fixed some errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Header from './Components/Header';
 import Home from './modules/Home';
 import Footer from './Components/Footer';
 import { Routes, Route } from 'react-router-dom';
-import Product from './Components/Product';
+import Product from './modules/Product';
+import Products from './modules/Products';
+import CategoryProducts from './modules/CategoryProducts';
+import Cart from './modules/Cart';
 
 const App = () => {
   return (
@@ -13,6 +16,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/products/:id' element={<Product/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/categories/:name' element={<CategoryProducts/> } />
+        <Route path='cart' element={<Cart/>} />
+        <Route path='*' element={<div>404</div>} />
       </Routes>
       <Footer />
     </div>
--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Hero  from '../../Components/Hero';
-import Products from '../../Components/Products';
+import Products from '../../Components/ProductCard';
 import FeatureCard from '../../Components/FeatureCard';
 import StatCard from '../../Components/StatCard';
+import ProductCard from '../../Components/ProductCard';
+import Categories from '../../Components/Categories';
 
 
 const Home = () => {
@@ -20,18 +22,18 @@ const Home = () => {
   return (
     <>
     <Hero />
+    <Categories />
     <div className='flex flex-col text-center w-full mt-20'>
       <h2 className='text-xs text-indigo-500 tracking-widest font-medium title-font mb-1'>Products</h2>
       <h1 className='sm:text-3xl text-2xl font-medium title-font text-gray-900'>Most Popular Products</h1>
     </div>
     {
       products.length > 0 ?
-      <Products products={products} />
+      <ProductCard products={products} />
       :
       <div>Loading....</div>
     }
     <Products />
-    <FeatureCard />
     <StatCard />
     </>
   )
